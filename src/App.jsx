6d350c2dec1bd1e9d8a3b38bd40506a01adfb1f8
Vitalii Hrebennikov <webasman@gmail.com>
2023-11-01 19:10:24 +0200

    add template for game blocks

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,21 @@ function App() {
   
 
   return (
-    <>
-    </>
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <li>
+            <span className="player-name">Player 1</span>
+            <span className="plyer-symbol">X</span>
+          </li>
+          <li>
+            <span className="player-name">Player 2</span>
+            <span className="plyer-symbol">O</span>
+          </li>
+        </ol>
+        GAME BOARD
+      </div>
+    </main>
   )
 }
 
