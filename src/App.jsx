c2341115eefd8c2f3b4388a71bb872c72f16700f
Vitalii Hrebennikov <webasman@gmail.com>
2023-11-04 20:45:03 +0200

    find out the winner

--- src/App.jsx
+++ src/App.jsx
@@ -18,6 +18,36 @@ function deriveActivePlayer(prevTurns) {
 
 function App() {
   const [gameTurns, setGameTurns] = useState([]);
+  const initialBoard = [
+    [null, null, null],
+    [null, null, null],
+    [null, null, null]
+  ];
+
+  let gameBoard = initialBoard;
+
+  for (const turn of gameTurns) {
+    const {square, player} = turn;
+    const {row, col} = square;
+
+    gameBoard[row][col] = player;
+  }
+
+  let winner;
+
+  for (const combination of WINNING_COMBINATIONS) {
+    const firstSquareSymbol = gameBoard[combination[0].row][combination[0].column];
+    const secondSquareSymbol = gameBoard[combination[1].row][combination[1].column];
+    const thirdSquareSymbol = gameBoard[combination[2].row][combination[2].column];
+
+    if (
+      firstSquareSymbol &&
+      firstSquareSymbol === secondSquareSymbol &&
+      firstSquareSymbol === thirdSquareSymbol
+    ) {
+      winner = firstSquareSymbol;
+    }
+  }
 
   const currentPlayer = deriveActivePlayer(gameTurns)
 
@@ -39,9 +69,10 @@ function App() {
           <Player initialName="Player 1" symbol="X" isActive={currentPlayer === 'X'}/>
           <Player initialName="Player 2" symbol="O" isActive={currentPlayer === 'O'}/>
         </ol>
+        <p>{winner && `Won player ${winner}`}</p>
         <GameBoard
           onSelectSquare={handleSelectSquare}
-          turns={gameTurns}
+          board={gameBoard}
         />
       </div>
       <Log turns={gameTurns}/>
